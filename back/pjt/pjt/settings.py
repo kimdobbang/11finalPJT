b1229c24ba690b6c5754d67d0c79eb6c372b3a02
cmh <zzckalsgur@koreatech.ac.kr>
2024-05-17 09:04:59 +0900

    [BE] Chore: django 환경 설정

--- back/pjt/pjt/settings.py
+++ back/pjt/pjt/settings.py
@@ -15,6 +15,21 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
+import environ
+
+# 환경변수를 불러올 수 있는 상태로 설정
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Take environment variables from .env file(환경변수 읽어올 파일 설정)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# 환경변수 읽어오기(기본 사용방법)
+os.environ.get('API_KEY')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
