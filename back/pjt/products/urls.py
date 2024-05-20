c92a2c75d368198e166968075421a7305bf30667
kim doi <kimdoiary@gmail.com>
2024-05-21 06:58:53 +0900

    [BE] Feat: 금융상품 상세조회 api 구현, Refactor: 게시판 HTTP 상태코드 update, Comment: product view 함수 주석 상세화

--- back/pjt/products/urls.py
+++ back/pjt/products/urls.py
@@ -3,11 +3,17 @@ from . import views
 
 app_name = 'products'
 urlpatterns = [
+    
+    # ----------- 저장 -----------
+    
     # [GET] 전체 정기예금 상품 및 상품 옵션 저장
     path('save-fixed/', views.save_fixed),
 
     # [GET] 전체 적금 상품 및 상품 옵션 저장
     path('save-installment/', views.save_installment),
+    
+    
+    # ----------- 조회 -----------
     
     # [GET] 전체 정기예금 상품 조회
     path('get-fixed/', views.get_fixed),
@@ -20,10 +26,19 @@ urlpatterns = [
     
     # [GET] 전체 적금 상품 옵션 조회
     path('get-installmentOption/', views.get_installmentOption),
+    
+    # [GET] 단일 정기예금 상품 상세조회
+    path('fixed/<int:product_id>/', views.detail_fixed),
 
+    # [GET] 단일 정기적금 상품 상세조회
+    path('installment/<int:product_id>/', views.detail_installment),
+    
+
+    # ----------- 상품가입 -----------
+    
     # [POST] 정기예금 상품 가입
     path('fixed/<str:username>/<int:product_id>/', views.join_fixed),
 
-    # [POST] 적금 상품 가입
+    # [POST] 정기적금 상품 가입
     path('installment/<str:username>/<int:product_id>/', views.join_installment),
 ]
