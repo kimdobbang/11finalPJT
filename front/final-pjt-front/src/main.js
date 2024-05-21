4fc4b4d6cc3fe12ec9630732308dba41a423c56c
01 <bubbleutopia@naver.com>
2024-05-21 15:41:17 +0900

    [FE] Feat : 예적금 목록 필터 은행제외 구현, 상품상세 페이지 테스트 위해 중간저장

--- front/final-pjt-front/src/main.js
+++ front/final-pjt-front/src/main.js
@@ -5,20 +5,12 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
 const app = createApp(App)
 const pinia = createPinia()
-const vuetify = createVuetify({
-  components,
-  directives,
-})
+
 pinia.use(piniaPluginPersistedstate)
 app.use(pinia)
 app.use(router)
-app.use(vuetify)
+
 
 app.mount('#app')
